e14d6b102af4a40bbc4e0082b37db88ba10ee1e6
mkmrah <39188713+mkmrah@users.noreply.github.com>
2020-10-22 13:14:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
-resource "ibm_iam_user_invite" "invite_user" {
+resource ibm_iam_user_invite "invite_user"{
+    depends_on = [null_resource.env_script]
     users = ["test@in.ibm.com"]
+}
+
+resource null_resource "env_script" {
+    provisioner local_exec {
+        command = "printenv"
+
+    }
 }
